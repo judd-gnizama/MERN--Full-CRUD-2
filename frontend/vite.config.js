191737c83952f557088f39df86d62c05de8f2397
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-29 03:57:10 +0800

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: process.env.NODE_ENV === 'development' ? {
-      '/api' : {
-        target: 'http://localhost:4000',
-        changeOrigin: true
-      }
-    } : {}
-  }
+  server: {}
 })
 
