a657b258d767f7ba8925802116ae02f1290878fe
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-29 03:16:50 +0800

    changed origin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const mode = process.env.NODE_ENV
 const corsOptions = mode === 'development' ? {
   origin: '*'
 } : {
-  origin: process.env.FRONTEND_URL_DEPLOY,
+  origin: "https://mern-full-crud-2-i1mc.vercel.app",
   methods: 'GET,POST,PUT,DELETE',  // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],  // Allowed headers
   credentials: true // Optional: Allow cookies or authorization credentials (if applicable)
